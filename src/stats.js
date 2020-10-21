10c207624bb8e9e834776c3f0bb110d29367f38e
vbstreetz <vbstreetz@gmail.com>
2020-10-21 18:44:00 +0300

    feat: refactor

--- src/stats.js
+++ src/stats.js
@@ -1,14 +1,16 @@
 const Redis = require("../src/redis")
 const moment = require("moment")
+const BN = require('bn.js');
+const Web3 = require("web3")
 
 const redis = Redis()
 
 module.exports = async function () {
-  const price = {}
-  price["1d"] = await get1d()
-  price["30d"] = await get30d()
-  price["all"] = await getAll()
-  return {price}
+  const data = {}
+  data["1d"] = await get1d()
+  data["30d"] = await get30d()
+  data["all"] = await getAll()
+  return data
 }
 
 async function get1d() {
@@ -47,16 +49,15 @@ async function getAll() {
 }
 
 async function get(intervals, getFromToForInterval) {
-  const x = []
-  const abs = []
-  const percentage = []
+  const xs = []
+  const prices = []
+  const supplies  = []
 
   for (i = 0; i < intervals; i++) {
-    const {x: _x, from, to} = getFromToForInterval(i)
-    // console.log(from.unix(), to.unix())
+    const {x, from, to} = getFromToForInterval(i)
 
-    let _abs = 0
-    let _percentage = 0
+    let price = bn(0)
+    let supply = bn(0)
     try {
       const blockNumbers = await redis("zrangebyscore", [
         "blocks",
@@ -64,23 +65,24 @@ async function get(intervals, getFromToForInterval) {
         to.unix(),
       ])
       const blocks = await redis("mget", blockNumbers)
-      _abs =
-        blocks
-          .map((b) => {
-            const {price} = JSON.parse(b)
-            return parseInt(price)
-          })
-          .reduce((o, a) => o + a, 0) / blocks.length
-      const _previousAbs = abs[abs.length - 1]
-      _percentage = _previousAbs
-        ? Math.ceil((100 * (_abs - _previousAbs)) / _abs)
-        : 0
+      blocks.forEach((b) => {
+        const {price: p, supply: s} = JSON.parse(b)
+        price = price.add(bn(p))
+        supply = supply.add(bn(s))
+      })
+      const len = bn(blocks.length.toString());
+      price = price.div(len)
+      supply = supply.div(len)
     } catch (e) {}
 
-    x.unshift(_x)
-    abs.unshift(_abs)
-    percentage.unshift(_percentage)
+    xs.unshift(x)
+    prices.unshift(Web3.utils.fromWei(price, 'ether').toString())
+    supplies.unshift(Web3.utils.fromWei(supply, 'gwei').toString())
   }
 
-  return {x, abs, '%': percentage}
+  return {x: xs, p: prices, s: supplies}
+}
+
+function bn(n) {
+  return new BN(n)
 }

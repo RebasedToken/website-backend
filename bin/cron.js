39035ecc47b25eb04a6f0c07d291f8d4c2a6bc62
vbstreetz <vbstreetz@gmail.com>
2020-10-27 22:07:53 +0300

    feat: increase cron frequency

--- bin/cron.js
+++ bin/cron.js
@@ -2,8 +2,8 @@ require("dotenv").config()
 const run = require("../src/sync-blocks")
 const cron = require("node-cron")
 
-// every 20 minutes
-cron.schedule("*/20 * * * *", async function () {
+// every 10 minutes
+cron.schedule("*/10 * * * *", async function () {
   console.log("running at", new Date())
   run(true)
 })

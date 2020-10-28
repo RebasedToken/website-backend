2034c6a17e3988f3e26f36cafff2cf564911863d
vbstreetz <vbstreetz@gmail.com>
2020-10-28 19:19:52 +0300

    feat: reduce cron time

--- bin/cron.js
+++ bin/cron.js
@@ -2,8 +2,8 @@ require("dotenv").config()
 const run = require("../src/sync-blocks")
 const cron = require("node-cron")
 
-// every 10 minutes
-cron.schedule("*/10 * * * *", async function () {
+// every 5 minutes
+cron.schedule("*/5 * * * *", async function () {
   console.log("running at", new Date())
   run(true)
 })
